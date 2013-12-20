9e205b97687f8219c9354979637def0bef0844ef
Christopher Armstrong <chris@chrisarmstrong.me>
2013-12-20 11:55:54 -0800

    Update maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "java-libraries"
 maintainer       "Socrata"
-maintainer_email "sysadmin@socrata.com"
+maintainer_email "chefs@socrata.com"
 license          "Apache 2.0"
 description      "Provides Java-related LWRPs and HWRPs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
