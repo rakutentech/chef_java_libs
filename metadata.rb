1359f79f917a0f65e2f34e9fac32dd7c7e14a17a
Christopher Armstrong <chris@chrisarmstrong.me>
2014-02-27 21:14:06 -0800

    Remove unused long_version

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,4 @@ maintainer       "Socrata, Inc."
 maintainer_email "chefs@socrata.com"
 license          "Apache 2.0"
 description      "Provides Java-related LWRPs and HWRPs"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.0"
